b6312748db0b20ce1937340438a5c4e8031d679c
Sam Heather <sam@heather.sh>
2014-12-11 02:30:08 +0000

    Added extra divs and works with added css from last commit

--- pygmaps.py
+++ pygmaps.py
@@ -33,10 +33,11 @@ class maps:
 		self.paths.append(path)
 	
 	#create the html file which inlcude one google map and all points and paths
-	def draw(self, htmlfile):
+	def draw(self, htmlfile, htmlForInsideTopDiv, htmlForInsideBottomDiv):
 		f = open(htmlfile,'w')
 		f.write('<html>\n')
 		f.write('<head>\n')
+		f.write('<link rel="stylesheet" href="style.css">\n')
 		f.write('<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />\n')
 		f.write('<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>\n')
 		f.write('<title>Google Maps - pygmaps </title>\n')
@@ -52,7 +53,13 @@ class maps:
 		f.write('</script>\n')
 		f.write('</head>\n')
 		f.write('<body style="margin:0px; padding:0px;" onload="initialize()">\n')
+		f.write('\t<div id="top_bar">')
+		f.write(htmlForInsideTopDiv)
+		f.write('</div>\n')
 		f.write('\t<div id="map_canvas" style="width: 100%; height: 100%;"></div>\n')
+		f.write('\t<div id="bottom_bar">')
+		f.write(htmlForInsideBottomDiv)
+		f.write('</div>\n')
 		f.write('</body>\n')
 		f.write('</html>\n')		
 		f.close()
